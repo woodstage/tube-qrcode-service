726b09b043f194a956511d8b185072a75d8bcba7
Jason Yin <jiangsheng.yin@hp.com>
2018-07-05 17:56:45 +0800

    update get image response

--- server.js
+++ server.js
@@ -1,31 +1,30 @@
 'use strict';
 
-const uuidv1 = require('uuid/v1');
-const Hapi=require('hapi');
+const uuidv4 = require('uuid/v4');
+const Hapi = require('hapi');
 
-const images = new Map();
+const images = {};
 
 // Create a server with a host and port
-const server=Hapi.server({
-    port: process.env.port
+const server = Hapi.server({
+  port: process.env.port || 8080
 });
 
 // Add the route
 server.route({
-    method:'GET',
-    path:'/hello',
-    handler:function(request,reply) {
-      
-        return'hello world';
-    }
+  method: 'GET',
+  path: '/hello',
+  handler: function (request, reply) {
+    return 'hello world';
+  }
 });
 
 server.route({
-  method:'POST',
-  path:'/images',
-  handler:function(request,h) {
+  method: 'POST',
+  path: '/images',
+  handler: function (request, h) {
     var value = request.payload.value;
-    var id = uuidv1();
+    var id = uuidv4();
     images[id] = value
     var data = {
       id: id,
@@ -36,17 +35,13 @@ server.route({
 });
 
 server.route({
-  method:'GET',
-  path:'/images/{id}',
-  handler:function(request,h) {
+  method: 'GET',
+  path: '/images/{id}',
+  handler: function (request, h) {
     var id = request.params.id;
     var value = images[id];
     if (!!id && !!value) {
-      var data = {
-        id: id,
-        value: value
-      }
-      return data;
+      return value;
     } else {
       return h.response().code(404)
     }
@@ -55,16 +50,14 @@ server.route({
 
 // Start the server
 async function start() {
+  try {
+    await server.start();
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
 
-    try {
-        await server.start();
-    }
-    catch (err) {
-        console.log(err);
-        process.exit(1);
-    }
-
-    console.log('Server running at:', server.info.uri);
+  console.log('Server running at:', server.info.uri);
 };
 
 start();
